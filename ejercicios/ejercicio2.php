793fe703432042434e5e46f420d6c89b69a291a5
Alex <alexpardo2006@gmail.com>
2025-10-23 10:00:49 +0200

    FEAT: Ejercicio 2.1 y 2.2 hecho

--- ejercicios/ejercicio2.php
+++ ejercicios/ejercicio2.php
@@ -111,7 +111,7 @@
       <!-- Fixed navbar -->
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <div class="container-fluid">
-          <a class="navbar-brand" href="..">Urtzi Diaz Arberas</a>
+          <a class="navbar-brand" href="..">Alex Pardo</a>
           <button
             class="navbar-toggler"
             type="button"
